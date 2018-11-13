2b09cd6632733791ddf03bbe50df78a7240b9e59
Casey Gorman <caseyagorman@gmail.com>
2018-11-13 09:40:03 -0800

    added nameChangedHanlder

--- src/App.js
+++ src/App.js
@@ -11,14 +11,24 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     // this.setState because this refers to the class that we're in, and setState
     // is a special function to change the state.
     this.setState({
       persons: [
-        { name: "Maximillian", age: 28 },
+        { name: newName, age: 28 },
         { name: "Casey", age: 33 },
         { name: "Stephen", age: 30 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 30 },
+        { name: "Stephen", age: 29 }
       ]
     });
   };
@@ -28,7 +38,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         {/* this.the function name but not calling it */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Maximilian!!")}>
+          Switch Name
+        </button>
 
         <Person
           name={this.state.persons[0].name}
@@ -37,6 +49,8 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
+          click={this.switchNameHandler.bind(this, "Max!")}
         />
         <Person
           name={this.state.persons[2].name}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,11 +5,11 @@ import React from "react";
 const person = props => {
   return (
     <div>
-      <p>
-        {/* can't use state in here cause it's just a function, not a class */}
+      <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} />
     </div>
   );
 };
